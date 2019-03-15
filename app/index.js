67db2d5d5284a401fd0d7e60e3879c1b7884ee82
aliaksandr.limasau <aliaksandr.limasau@bitbucket.wolterskluwer.io>
2019-03-15 13:35:19 +0300

    stats have been added to weapon definition

--- app/index.js
+++ app/index.js
@@ -12,17 +12,23 @@ console.log('let\'s get start'.yellow);
 console.log('weapon starts are downloading...'.yellow);
 console.time('completed');
 
+// stats request
 
 fetch(manifestProperties.statDefinition.url)
-    .then((response)=>{
+    .then((response) => {
         console.log('stats are downloaded'.yellow);
-        
+
         return response.json();
     })
-    .then((body)=>{
+    .then((body) => {
         fs.createWriteStream(`destination/${manifestProperties.statDefinition.name}.json`).write(JSON.stringify(body));
-    }).then(()=>{
+
+        return body;
+    }).then((stats) => {
         console.log('weapon definition is downloading...'.yellow);
+
+        // definition request
+
         fetch(manifestProperties.inventoryItemDefinition.url)
             .then((response) => {
 
@@ -34,10 +40,14 @@ fetch(manifestProperties.statDefinition.url)
 
                 let reducedWeapon = [];
                 for (let item in body) {
+
+                    // cycle to iterate the weapon item type
+
                     for (let classItem of weaponMap.classes) {
                         if (body[item].itemTypeDisplayName === classItem) {
                             try {
                                 let displayedPropertyObject = body[item].displayProperties;
+                                let statsArray = [];
 
                                 let reducedWeaponDescription = {
                                     displayedProperties: {
@@ -51,9 +61,28 @@ fetch(manifestProperties.statDefinition.url)
                                     }
                                 };
 
+                                try {
+                                    for (let stat in body[item].stats.stats) {
+                                        if (stats[stat].displayProperties.name) {
+                                            statsArray.push({
+                                                statName: stats[stat].displayProperties.name,
+                                                statValue: body[item].stats.stats[stat].value,
+                                                minValue: body[item].stats.stats[stat].minimum,
+                                                maxValue: body[item].stats.stats[stat].maximum
+                                            });
+                                        }
+                                    };
+                                } catch (error) {
+                                    console.log('error in stats level'.red);
+                                    console.log(error.message);
+                                };
+
+                                reducedWeaponDescription.description.stats = statsArray;
+
                                 reducedWeapon.push(reducedWeaponDescription);
                             } catch (error) {
-                                console.log('item dosen\'t parsed'.red);
+                                console.log('error in displayed properties level'.red);
+                                console.log(error.message);
                             };
                         }
                     };
@@ -67,9 +96,8 @@ fetch(manifestProperties.statDefinition.url)
             .catch((error) => {
                 console.log(error.message.red);
             });
-        
+
     })
     .catch((error) => {
         console.log(error.message.red);
     });
-
